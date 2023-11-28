78ea714fc7b34b1760864e50aa4fd82b82f19cee
Slavutych42 <128924265+Slavutych42@users.noreply.github.com>
2023-11-28 18:21:43 +0200

    done

--- src/class/user.js
+++ src/class/user.js
@@ -30,7 +30,20 @@ class User {
   static create = (data) => {
     const user = new User(data)
 
+    console.log(user)
+
     this.#list.push(user)
+
+    console.log(this.#list)
+  }
+
+  static getByEmail(email) {
+    return (
+      this.#list.find(
+        (user) =>
+          user.email === String(email).toLowerCase(),
+      ) || null
+    )
   }
 }
 module.exports = {
--- src/route/auth.js
+++ src/route/auth.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@email.com',
+  password: 123,
+  role: 1,
+})
+
 router.get('/signup', function (req, res) {
   return res.render('signup', {
     name: 'signup',
@@ -30,6 +36,43 @@ router.get('/signup', function (req, res) {
       ],
     },
   })
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+
+    if (user) {
+      return res.status(400).json({
+        message: 'Помилка. Такий користувач вже існує',
+      })
+    }
+
+    const newUser = User.create({ email, password, role })
+
+    const session = Session.create(newUser)
+
+    Confirm.create(newUser.email)
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрованний',
+      session,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача',
+    })
+  }
 })
 
 module.exports = router
