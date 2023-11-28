ed0ddaba0c29298ec9e54404af2a64324cdd4022
Slavutych42 <128924265+Slavutych42@users.noreply.github.com>
2023-11-28 16:30:18 +0200

    q

--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,18 @@
-// Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-const test = require('./test')
-// Підключіть інші файли роутів, якщо є
+router.get('/', function (req, res) {
+  res.render('index', {
+    name: 'index',
+    component: [''],
+    title: 'Home page',
 
-// Об'єднайте файли роутів за потреби
-router.use('/', test)
-// Використовуйте інші файли роутів, якщо є
+    data: {},
+  })
+})
 
-// Експортуємо глобальний роутер
+const auth = require('./auth')
+
+router.use('/', auth)
+
 module.exports = router
